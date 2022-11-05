32f9a961c331d5616d1c137fd0c02140b35ab032
hu1won <ehql713@gmail.com>
2022-11-05 14:26:10 +0900

    urls init

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    path('', views.main_page, name= 'main_page'),
+    path('login/', views.login_page, name='login_page'),
+    path('login/validate', views.login_validate, name='login_validate'),
+    path('logout/', views.logout, name='logout'),
+    path('join/', views.join_page, name='join_page'),
+    path('edit/', views.edit_user_info, name='edit_user_info'),
+]
